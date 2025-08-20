26a03d64e6aed197d0b6d0a94445e33ff9754a18
kkotdari <kkotdari@gmail.com>
2025-08-20 09:40:20 +0900

    로그인 로그아웃 함수 수정

--- src/main/resources/static/js/auth/index.js
+++ src/main/resources/static/js/auth/index.js
@@ -1,8 +1,54 @@
-const login = async () => {
+signup = async () => {
+    const form = document.getElementById('signup-form');
+    const formData = new FormData(form)
 
+    fetch('auth/signup', {
+        method: 'POST',
+        body: formData
+    })
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Signup failed')
+            }
+            return res.text()
+        })
+        .then(html => {
+            document.getElementById('content-container').innerHTML = html
+        })
+        .catch(err => {
+            console.error('auth > signup > signup error: ', err)
+        })
 }
 
-const logout = async () => {
+login = async () => {
+    const form = document.getElementById('login-form')
+    const formData = new FormData(form)
+
+    try {
+        const indexRes = await fetch('/auth/login', {
+            method: 'POST',
+            body: formData
+        })
+
+        if (!indexRes.ok) {
+            console.error('auth > login > indexRes: ', indexRes)
+        }
+
+        const indexResText = await indexRes.text()
+
+        console.log('auth > login > indexResText: ', indexResText)
+        document.getElementById('content-container').innerHTML = indexResText
+
+        const headerRes = await fetch('header')
+        const headerResText = await headerRes.text()
+        const headerEl = document.getElementById('header');
+        headerEl.outerHTML = headerResText;
+    } catch (err) {
+        console.error('auth > login > login error: ', err)
+    }
+}
+
+logout = async () => {
     console.log('logout')
 
     try {
@@ -24,7 +70,6 @@ const logout = async () => {
         const headerResText = await headerRes.text()
         const headerEl = document.getElementById('header');
         headerEl.outerHTML = headerResText;
-        initListeners('index')
     } catch (err) {
         console.error('auth > logout > logout error: ', err)
     }
